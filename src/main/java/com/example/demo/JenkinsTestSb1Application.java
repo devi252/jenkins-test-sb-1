8db45c6a97fc57a0437a4dcfb3ca6e5252feda02
devi252 <k.naveen252@gmail.com>
2024-06-20 17:27:31 +0530

    first commit

---
+++ src/main/java/com/example/demo/JenkinsTestSb1Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsTestSb1Application {
+
+	@GetMapping("/home")
+	public String home() {
+		return "this endpoint is working fine..........";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTestSb1Application.class, args);
+	}
+
+}
